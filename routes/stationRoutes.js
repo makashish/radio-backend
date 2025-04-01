0fb6ddd67762094be99897f2dc3f0e8270a3cd0c
ASHISH <makanindian@gmail.com>
2025-04-01 09:30:59 +0530

    Updated MongoDB integration

--- routes/stationRoutes.js
+++ routes/stationRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Fetch all stations
 router.get("/", async (req, res) => {
   try {
-    const stations = await Station.find({}, { __v: 0 }); // Exclude version key
+    const stations = await Station.find({}, { _id: 0, name: 1, url: 1}); // Exclude version key
     res.json(stations);
   } catch (error) {
     console.error("Error fetching stations:", error);
